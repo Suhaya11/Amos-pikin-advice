f21f14ade525f6fb678c43adbaa0c598724c8289
suhaya11 <sulaimanhaladukadigawakibiya@gmail.com>
2026-03-04 05:50:07 -0600

    some features added

--- app/daily-wahala/history/page.tsx
+++ app/daily-wahala/history/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Data, myData, wahala } from "@/src/components/data";
 import React from "react";
-import { BiVolumeFull } from "react-icons/bi";
+import { BiPause, BiPlay, BiStop, BiVolumeFull } from "react-icons/bi";
 
 const WahalTrackerHomePage = () => {
   const [wahalas, setWahalas] = React.useState<wahala[]>([
@@ -30,6 +30,10 @@ const WahalTrackerHomePage = () => {
       rating: "difficult",
     },
   ]);
+  const [audioControlsOpen, setAudioControlsOpen] =
+    React.useState<boolean>(true);
+  const [loudReading, setLoudReading] = React.useState<true | false>(true);
+  const [speechStarted, setSpeechStarted] = React.useState<boolean>(false);
   React.useEffect(() => {
     const queryLocalStorage = localStorage.getItem("AmosIdeaApp");
     if (queryLocalStorage) {
@@ -37,6 +41,8 @@ const WahalTrackerHomePage = () => {
       // if (localData?.daily_wahala) setWahalas(myData.daily_wahala as wahala[]);
     }
   }, []);
+  const sayLaud = (text: string) =>
+    speechSynthesis.speak(new SpeechSynthesisUtterance(text));
 
   return (
     <div>
@@ -45,13 +51,30 @@ const WahalTrackerHomePage = () => {
           key={wahala.id}
           className="flex text-white relative flex-row flex-wrap gap-3 justify-center border my-2 mx-auto w-11/12 p-2 from-green-700 to-amber-500 cursor-default bg-linear-to-r rounded-xl "
         >
-          <BiVolumeFull
-            className="absolute right-4"
-            title="Read summary of this wahala laudly"
-            onClick={() => {
-              if (!speechSynthesis.speaking && !speechSynthesis.paused)
-                speechSynthesis.speak(
-                  new SpeechSynthesisUtterance(`
+          <div className="absolute flex  right-4">
+            {" "}
+            <BiStop
+              onClick={() => {
+                speechSynthesis.cancel();
+
+                setSpeechStarted(false);
+              }}
+            />
+            {speechStarted ? (
+              <BiPause
+                onClick={() => {
+                  speechSynthesis.pause();
+                  setSpeechStarted(false);
+                }}
+              />
+            ) : (
+              <BiPlay
+                className=" right-4"
+                title="Read summary of this wahala laudly"
+                onClick={() => {
+                  if (!speechSynthesis.speaking)
+                    speechSynthesis.speak(
+                      new SpeechSynthesisUtterance(`
               Oh! user this here's breakdown of this wahala. Name. ${wahala.name} . which occured on ${new Date(wahala.date).toDateString()} .  discription of this wahala is. ${wahala.desctiption}
 
               
@@ -64,31 +87,41 @@ const WahalTrackerHomePage = () => {
               
               
               `),
-                );
-              else if (speechSynthesis.speaking) speechSynthesis.pause();
-              else if (speechSynthesis.paused) speechSynthesis.resume();
-            }}
-            add
-            puse
-            and
-            continye
-            buttons
-            to
-            the
-            speechsyntheis
-            feature
-            here
-          />
-          <h1 className="title text-white w-full">{wahala.name}</h1>
-          <h3 className=" right-2 text-right mr-3  top-14 font-bold hover:p-1 w w-full">
+                    );
+                  else if (speechSynthesis.paused) speechSynthesis.resume();
+                  setSpeechStarted(true);
+                }}
+              />
+            )}
+          </div>
+
+          <h1
+            onClick={(e) => loudReading && sayLaud(e.currentTarget.textContent)}
+            className="title text-white w-full"
+          >
+            {wahala.name}
+          </h1>
+          <h3
+            onClick={(e) => loudReading && sayLaud(e.currentTarget.textContent)}
+            className=" right-2 text-right mr-3  top-14 font-bold hover:p-1 w w-full"
+          >
             {new Date(wahala.date).getDate() === new Date().getDate() &&
             new Date(wahala.date).getMonth() === new Date().getMonth() &&
             new Date(wahala.date).getFullYear() === new Date().getFullYear() ? (
-              <span className="mask-b-from-80% mask-l-from-90 border-3  p-1 rounded-md">
+              <span
+                onClick={(e) =>
+                  loudReading && sayLaud(e.currentTarget.textContent)
+                }
+                className="mask-b-from-80% mask-l-from-90 border-3  p-1 rounded-md"
+              >
                 Today
               </span>
             ) : (
-              <span>
+              <span
+                onClick={(e) =>
+                  loudReading && sayLaud(e.currentTarget.textContent)
+                }
+              >
                 {new Date(wahala.date).getDate() === new Date().getDate() - 1 &&
                 new Date(wahala.date).getMonth() === new Date().getMonth() &&
                 new Date(wahala.date).getFullYear() ===
@@ -97,7 +130,12 @@ const WahalTrackerHomePage = () => {
                     Yesterday
                   </span>
                 ) : (
-                  <span className="mask-b-from-80% mask-l-from-90 border-3  p-1 rounded-md">
+                  <span
+                    onClick={(e) =>
+                      loudReading && sayLaud(e.currentTarget.textContent)
+                    }
+                    className="mask-b-from-80% mask-l-from-90 border-3  p-1 rounded-md"
+                  >
                     {new Date(wahala?.date)?.getDate()}/
                     {new Date(wahala.date)?.getMonth()}/
                     {new Date(wahala.date).getFullYear()}
@@ -106,22 +144,52 @@ const WahalTrackerHomePage = () => {
               </span>
             )}
           </h3>
-          <p>{wahala.desctiption}</p>
+          <p
+            onClick={(e) => loudReading && sayLaud(e.currentTarget.textContent)}
+          >
+            {" "}
+            {wahala.desctiption}
+          </p>
           <div className="flex gap-1  text-white font-serif w-full">
             <h2 className="font-extrabold">Decision:</h2>
-            <p className="w-full inline-block ">{wahala.decision}</p>
+            <p
+              onClick={(e) =>
+                loudReading && sayLaud(e.currentTarget.textContent)
+              }
+              className="w-full inline-block "
+            >
+              {wahala.decision}
+            </p>
           </div>
           <div className="w-full flex gap-1 text-gray-50 ">
             <h3 className="font-extrabold">Level:</h3>{" "}
-            <span>{wahala.rating}</span>
+            <span
+              onClick={(e) =>
+                loudReading && sayLaud(e.currentTarget.textContent)
+              }
+            >
+              {wahala.rating}
+            </span>
           </div>
           <div className="w-full text-amber-50 flex gap-2">
             <h3 className="font-extrabold">Status</h3>
-            <span>{wahala.status}</span>
+            <span
+              onClick={(e) =>
+                loudReading && sayLaud(e.currentTarget.textContent)
+              }
+            >
+              {wahala.status}
+            </span>
           </div>
           <div className="w-full flex gap-2 text-blue-50">
             <h3 className="  font-extrabold">Caused by:</h3>
-            <span>{wahala.causer}</span>
+            <span
+              onClick={(e) =>
+                loudReading && sayLaud(e.currentTarget.textContent)
+              }
+            >
+              {wahala.causer}
+            </span>
           </div>
         </div>
       ))}
