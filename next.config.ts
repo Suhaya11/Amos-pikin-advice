dfb165260ba5ef9ab299eb9a79a52c13e371e2cb
suhaya11 <sulaimanhaladukadigawakibiya@gmail.com>
2026-06-15 14:05:25 -0500

    I'm working on about page

--- next.config.ts
+++ next.config.ts
@@ -14,10 +14,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",
+  // output: "export", uncomment for folder base out after build
 
   // Placed at the root level as required by Next.js
   allowedDevOrigins: ["local-origin.dev", "*", "*.local-origin.dev"],
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
